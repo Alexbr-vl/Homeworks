1d824c6f6cf4f808e19098485f0a5a052c0c05f1
Alexbr-vl <alexbr-vl@yandex.ru>
2024-11-28 22:55:05 +1000

    Del LEAVER_EXPORTS and LEAVER_API, include set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)

--- Homework 11_02/LeaverLibrary/Leaver.h
+++ Homework 11_02/LeaverLibrary/Leaver.h
@@ -1,14 +1,7 @@
 #pragma once
-
-#ifdef LEAVER_EXPORTS
-#define LEAVER_API __declspec(dllexport)
-#else
-#define LEAVER_API __declspec(dllimport)
-#endif
-
 #include <string>
 
-class LEAVER_API Leaver {
+class Leaver {
 public:
     std::string leave(const std::string& name) const;
 };
