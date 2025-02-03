92c5a0a407e81d5db75c4bdba9e38168a41bf8d9
Alexbr-vl <alexbr-vl@yandex.ru>
2025-02-03 20:14:13 +1000

    Initial commit

---
+++ Homework05_01_the_squaring_pattern/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "pattern.h"  // Подключаем заголовочный файл с шаблонными функциями
+
+int main() {
+    int num = 4;
+    std::cout << "[IN]: " << num << std::endl;
+    std::cout << "[OUT]: " << square(num) << std::endl;
+
+    std::vector<int> my_vector = { -1, 4, 8 };
+    std::cout << "[IN]: ";
+    printVector(my_vector);
+
+    std::vector<int> result_vector = square(my_vector);
+    std::cout << "[OUT]: ";
+    printVector(result_vector);
+}
---
+++ Homework05_01_the_squaring_pattern/pattern.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+// Общий шаблон возведения в квадрат
+template<typename T>
+T square(T x) 
+{
+    return x * x;
+}
+
+// перегрузка шаблона для std::vector<T>
+template<typename T>
+std::vector<T> square(const std::vector<T>& vector) 
+{
+    std::vector<T> result;
+    for (const T& num : vector) //перебираем каждый элемент вектора и ложим его квадрат в новый вектор
+    {
+        result.push_back(num * num);
+    }
+    return result;
+}
+
+// шаблон вывода вектора в консоль
+template<typename T>
+void printVector(const std::vector<T>& vector) 
+{
+    for (size_t i = 0; i < vector.size(); i++) 
+    {
+        std::cout << vector[i] << (i + 1 < vector.size() ? ", " : "\n");
+    }
+}
