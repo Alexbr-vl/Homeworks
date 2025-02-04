fa72f1baa01928d6b52a8e25647285060a72b8e8
Alexbr-vl <alexbr-vl@yandex.ru>
2025-02-04 15:42:33 +1000

    Initial commit

---
+++ Homework05_02_the_table_pattern/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "pattern.h"  // Подключаем заголовочный файл с шаблонными функциями
+
+int main() 
+{
+    Table<int> table(2, 3); // Создаём таблицу 2x3
+    table[0][0] = 4;        // Записываем значение
+    std::cout << table[0][0] << std::endl; // Выводим значение table[0][0] в консоль
+
+    size_t rows, cols;
+    table.Size(rows, cols);
+    std::cout << "Size: " << rows << "x" << cols << std::endl; // Вывод размера таблицы
+}
---
+++ Homework05_02_the_table_pattern/pattern.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+// Шаблон класса
+template <typename T>
+class Table 
+{
+public:
+    // Конструктор, создающий таблицу с заданным количеством строк и столбцов
+    Table(size_t rows, size_t cols) : data(rows, std::vector<T>(cols)) {}
+
+    // Неконстантный оператор [] для изменяемых таблиц
+    std::vector<T>& operator[](size_t index) 
+    {
+        return data[index];
+    }
+
+    // Константный оператор [] для неизменяемых таблиц
+    const std::vector<T>& operator[](size_t index) const 
+    {
+        return data[index];
+    }
+
+    void Size(size_t& rows, size_t& cols) const
+    {
+        rows = data.size();
+
+        if (data.empty()) 
+        {
+            cols = 0;
+        }
+        else 
+        {
+            cols = data[0].size();
+        }
+    }
+
+private:
+    std::vector<std::vector<T>> data; // Двумерный массив, список списков, она же таблица
+};
