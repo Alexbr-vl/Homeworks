26b9dec64099c55ff4530701fecc6ece9d21b635
Alexbr-vl <alexbr-vl@yandex.ru>
2025-02-04 16:14:09 +1000

    Initial commit

---
+++ Homework05_03_functor_counter/functor.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+class Counter
+{
+private:
+    int sum = 0;     // переменная для суммы чисел
+    int count = 0;   // переменная для количества чисел делящихся на три
+
+public:
+    void operator()(int number) //перегрузка оператора ()
+    {
+        if (number % 3 == 0) // Если число делится на 3 без остатка
+        {
+            sum += number;
+            count++;
+        }
+    }
+    int get_sum() const { return sum; }
+    int get_count() const { return count; }
+};
+
+// шаблон вывода вектора в консоль
+template<typename T>
+void printVector(const std::vector<T>& vector)
+{
+    for (size_t i = 0; i < vector.size(); i++)
+    {
+        std::cout << vector[i] << (i + 1 < vector.size() ? ", " : "\n");
+    }
+}
---
+++ Homework05_03_functor_counter/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "functor.h"  // Подключаем заголовочный файл с шаблонными функциями
+
+int main() 
+{
+    Counter counter;
+    std::vector<int> numbers{ 4, 1, 3, 6, 25, 54 };
+    std::cout << "[IN]: "; 
+    printVector(numbers);
+    
+    for (int number : numbers) // Вызываем функтор для каждого числа
+    {
+        counter(number);  
+    }
+
+    std::cout << "[OUT]: get_sum() = " << counter.get_sum() << "\n";
+    std::cout << "[OUT]: get_count() = " << counter.get_count() << "\n";
+}
