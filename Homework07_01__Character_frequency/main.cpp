6d7b81aca5bec7fe5ecc475476449c8c1a7cd2bb
Alexbr-vl <alexbr-vl@yandex.ru>
2025-02-11 18:08:37 +1000

    Initial commit

---
+++ Homework07_01__Character_frequency/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <string>
+
+int main()
+{
+    std::string text;
+    std::cout << "[IN]: ";
+    std::getline(std::cin, text);  // Считываем строку целиком
+
+    std::unordered_map<char, int> freq;  // Хэш-таблица для подсчёта частоты
+
+    // Подсчёт частоты символов (игнорируем пробелы)
+    for (char c : text)
+    {
+        if (c != ' ')
+        {
+            freq[c]++;
+        }
+    }
+
+    // multimap автоматически сортирует по убыванию частоты
+    std::multimap<int, char, std::greater<int>> sorted_freq;
+
+    // Заполняем multimap (меняем местами ключ и значение)
+    for (const auto& pair : freq)
+    {
+        sorted_freq.insert({ pair.second, pair.first });
+    }
+
+    std::cout << "[OUT]:\n";
+    // Выводим отсортированные символы
+    for (const auto& pair : sorted_freq)
+    {
+        std::cout << pair.second << ": " << pair.first << "\n";
+    }
+}
