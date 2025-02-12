0232258d680674148f6adb69d2900fc80088b3d8
Alexbr-vl <alexbr-vl@yandex.ru>
2025-02-12 19:13:50 +1000

    Initial commit

---
+++ Homework07_02_removing_duplicates/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+
+int main() 
+{
+    int x;
+    std::cout << "IN: " << std::endl;
+    std::cout << "enter the number of items: ";  
+    std::cin >> x; // Считываем количество элементов
+
+    std::set<int> uniq_numbers; // Используем set для хранения уникальных чисел
+    for (int i = 0; i < x; ++i) 
+    {
+        int num;
+        
+        std::cout << "enter the (" << i+1 << ") element: ";
+        std::cin >> num;
+        uniq_numbers.insert(num); // Добавляем числа в set (дубликаты игнорируются)
+    }
+
+    // Выводим элементы set в обратном порядке
+    std::cout << "\nOUT: " << std::endl;
+    for (auto it = uniq_numbers.rbegin(); it != uniq_numbers.rend(); ++it) 
+    {
+        std::cout << *it << std::endl;
+    }
+}
